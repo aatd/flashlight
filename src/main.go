681f84a7b206290f9af718e4613dff7beb9c870c
aduendar <aduendar@cad-schroer.de>
2020-09-28 14:08:43 +0200

    Changed project strucutre and moved some util func's

---
+++ src/main.go
@@ -0,0 +1,22 @@
+/*
+ * Flashlight - By Asef Alper Tunga Dündar
+ *
+ * This Appication is something like Instagram implemented for the Cologne University of Applied Sciences
+ *
+ * API version: 1.0.0
+ */
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"controller"
+)
+
+func main() {
+	log.Printf("Server started")
+	router := controller.CreateRouter()
+	log.Fatal(http.ListenAndServe(":80", router))
+}
